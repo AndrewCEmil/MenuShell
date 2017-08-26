5246f8e12402147dd276848fece95f0f376e7b4f
ace <andrew.emil@gmail.com>
2017-08-26 15:44:41 -0400

    Got the shell of all scenes created

---
+++ Assets/Scripts/MenuController.cs
@@ -0,0 +1,64 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class MenuController : MonoBehaviour {
+
+	public Slider musicSlider;
+	public Toggle particleToggle;
+	// Use this for initialization
+	void Start () {
+		MaybeInitializePlayerPrefs ();
+	}
+
+	// Update is called once per frame
+	void Update () {
+
+	}
+
+	public void LoadGame() {
+		Application.LoadLevel("DemoScene");
+	}
+
+	public void LoadAnteChamber() {
+		Application.LoadLevel("AntechamberScene");
+	}
+
+	public void LoadSettings() {
+		Application.LoadLevel ("SettingsScene");
+	}
+
+	public void LoadStart() {
+		Application.LoadLevel ("StartScene");
+	}
+
+	public void LoadAboutScene() {
+		Application.LoadLevel ("AboutScene");
+	}
+
+	public void Quit() {
+		PlayerPrefs.Save ();
+		Application.Quit ();
+	}
+
+	public void SetMusicVolume() {
+		SoundController soundController = GameObject.Find ("MusicPlayer").GetComponent<SoundController> ();
+		soundController.SetMusicVolume (musicSlider.value);
+	}
+
+	//Reversed so we default to particles on
+	public void SetParticleToggle() {
+		if (particleToggle.isOn) {
+			PlayerPrefs.SetInt ("ParticlesOff", 0);
+		} else {
+			PlayerPrefs.SetInt ("ParticlesOff", 1);
+		}
+		PlayerPrefs.Save ();
+	}
+
+	private void MaybeInitializePlayerPrefs() {
+		//Other settings
+	}
+
+	//TODO initialize settings menu
+}
---
+++ Assets/Scripts/SoundController.cs
@@ -0,0 +1,55 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SoundController : MonoBehaviour {
+
+	//TODO everything for sound effects
+
+	private static SoundController instance = null;
+	private AudioSource audioSource;
+	private float defaultMusicVolume;
+	public static SoundController Instance {
+		get { return instance; }
+	}
+	void Awake() {
+		if (instance != null && instance != this) {
+			Destroy(this.gameObject);
+			return;
+		} else {
+			instance = this;
+		}
+		DontDestroyOnLoad(this.gameObject);
+		audioSource = GetComponent<AudioSource> ();
+		defaultMusicVolume = .5f;
+
+		InitializeSound ();
+	}
+
+	private void InitializeSound() {
+		audioSource.volume = getSavedMusicVolume ();
+	}
+
+	// Update is called once per frame
+	void Update () {
+
+	}
+
+	public void SetMusicVolume(float volume) {
+		audioSource.volume = volume;
+		saveMusicVolume(volume);
+	}
+
+	private void saveMusicVolume(float volume) {
+		PlayerPrefs.SetFloat ("musicvolume", volume);
+		PlayerPrefs.Save ();
+	}
+
+	private float getSavedMusicVolume() {
+		float volume = PlayerPrefs.GetFloat ("musicvolume", -1);
+		if (volume < 0) {
+			volume = defaultMusicVolume;
+			saveMusicVolume (volume);
+		}
+		return volume;
+	}
+}
